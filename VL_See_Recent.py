dc014a6afda559f276097bc8419f31f1ce46a5c8
northloopforensics <73806121+northloopforensics@users.noreply.github.com>
2021-10-26 10:08:19 -0500

    Update VL_See_Recent.py

--- VL_See_Recent.py
+++ VL_See_Recent.py
@@ -1,6 +1,5 @@
 #! python3
 # VL_SEE_RECENT
-# Copyright 2021 North Loop Consulting, LLC
 
 from __future__ import print_function
 import argparse
@@ -16,7 +15,7 @@ _description_ = ("---VL_See_Recent v1.0---\n"
                  " A tool to retrieve recent file activity from VLC configuration ini file."" \n "
                  )
 now = datetime.now()
-now = now.strftime('%Y-%m-%d %H:%m:%d')
+now = now.strftime('%Y-%m-%d %H:%M:%S')
 parser = argparse.ArgumentParser(
     description=_description_,
     epilog="{}".format(
@@ -24,7 +23,7 @@ parser = argparse.ArgumentParser(
 
 #Add positional arguments
 parser.add_argument("INPUT_VOLUME", help="Input volume letter - ex. 'C:' or Absolute path - ex. 'E:\\Evidence\\KapeCollection\\C'")
-parser.add_argument("OUTPUT_FOLDER", help="Path to store report text file")
+parser.add_argument("OUTPUT_FOLDER", help="Path to store file")
 
 # Optional Arguments
 #Parsing and using the arguments
@@ -34,16 +33,24 @@ input_volume = args.INPUT_VOLUME
 output_folder = args.OUTPUT_FOLDER
 
 savePath = args.OUTPUT_FOLDER
-rptfile = (f"VL_See_Recent Report-{datetime.now():%Y-%m-%d %H-%m-%d}.txt")
+# rpt_name = "VL_See_Recent Report.txt"
+#completeName = (f"VL_See_Recent Report-{datetime.now():%Y-%m-%d %H-%M-%S}.txt")
+rptfile = (f"VL_See_Recent Report-{datetime.now():%Y-%m-%d %H-%M-%S}.txt")
 completeName = os.path.join(savePath, rptfile)
-
 org_stdout = sys.stdout
+
+print(completeName)
 
 with open(completeName, 'w') as report:
     sys.stdout = report
 
 #Parse ini file 
 def parse_INI():
+    INI1 = input_volume + ("\\Users\\*\\AppData\\Roaming\\vlc\\vlc-qt-interface.ini")
+    INI2 = input_volume + ("\\**\\Users\\*\\AppData\\Roaming\\vlc\\vlc-qt-interface.ini")    #Finds the ini file for any/all users using volume letter
+    Method1 = glob2.glob(INI1)
+    
+    
     
     with open(completeName, 'a') as report:     #print report header to file
         sys.stdout = report
@@ -59,16 +66,16 @@ def parse_INI():
         print("Report Generated with User Acct: ", os.getlogin())
         print("Report Generated on Host:  " + socket.gethostname(), "\n")
         sys.stdout = org_stdout
-
-    if len(input_volume) == 2:
-        INI2 = input_volume + ("\\Users\\*\\AppData\\Roaming\\vlc\\vlc-qt-interface.ini")    #Finds the ini file for any/all users using volume letter
+    if len(Method1) == 0:
+        Method1 = glob2.glob(INI2)
     else:
-        INI2 = input_volume + ("\\**\\Users\\*\\AppData\\Roaming\\vlc\\vlc-qt-interface.ini")
+        pass
 
-    for targetuser in glob2.glob(INI2):
+    for targetuser in Method1:
+        
         print(targetuser)
         mtime = os.path.getmtime(targetuser) #Gets last mod time for ini file
-                #print("File Last Modified: %s" % datetime.datetime.fromtimestamp(mtime), '\n')
+        #print("File Last Modified: %s" % datetime.datetime.fromtimestamp(mtime), '\n')
     
         modtime = datetime.fromtimestamp(mtime)
         modtime = modtime.strftime('%Y-%m-%d %H:%m:%d')
